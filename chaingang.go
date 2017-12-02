23254aedd31ce81d75f5f9075bee6d9b78c0a339
James Prillaman <jwprillaman@gmail.com>
2017-12-02 02:54:19 -0700

    added api call. Getting ticker for every open market

--- chaingang.go
+++ chaingang.go
@@ -6,9 +6,27 @@ import (
 	"github.com/toorop/go-bittrex"
 )
 
+func getBittrexMarketRecords(bittrexClient *bittrex.Bittrex, markets []bittrex.Market) []marketRecord{
+	marketRecords := []marketRecord{}
+	for _, market := range markets {
+		if(market.IsActive){
+			ticker, err := bittrexClient.GetTicker(market.MarketName);
+			if(err == nil){
+				record := marketRecord {
+					Market: market,
+					Ticker: ticker,
+				}
+				marketRecords = append(marketRecords, record)
+			} else {
+				fmt.Println(err)
+			}
+		}
+	}
+	return marketRecords
+}
+
 func main() {
 	var bittrexKey, coinbaseKey, bittrexSecret string
-	const maxArguments = 3
 	fmt.Printf("chaingang running\n")
 		for i := 1; i < len(os.Args); i += 2 {
 			switch os.Args[i]{
@@ -28,10 +46,26 @@ func main() {
 	fmt.Printf("\tcoinbaseKey: %v\n", coinbaseKey)
 
 	if(bittrexKey != "" && bittrexSecret != ""){
-		bittrex := bittrex.New(bittrexKey, bittrexSecret)
-		markets, err := bittrex.GetMarkets()
-		fmt.Println(err, markets)
+		bittrexClient := bittrex.New(bittrexKey, bittrexSecret)
+		markets, err := bittrexClient.GetMarkets()
+		if(err == nil){
+			//this is where we start looping updates
+			//might have to work with 'SubscribeExchangeUpdate'
+			marketRecords := getBittrexMarketRecords(bittrexClient, markets)
+			for _,record := range marketRecords {
+				fmt.Printf("%v\n", record.Market.MarketName)
+				fmt.Printf("\t %v\n", record.Ticker)
+			}
+		} else {
+			fmt.Println(err)
+		}
+
 	} else {
-		fmt.Printf("please provide bittrex key and secret")
+		fmt.Println("please provide bittrex key and secret")
 	}
+}
+
+type marketRecord struct {
+	Market bittrex.Market
+	Ticker bittrex.Ticker
 }
