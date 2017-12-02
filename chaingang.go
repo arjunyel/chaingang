cad1e7756a666c5f45dce5b1b7cd6caabacebd0b
James Prillaman <jwprillaman@gmail.com>
2017-12-02 00:17:40 -0700

    get keys as args

--- chaingang.go
+++ chaingang.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+)
 
 func main() {
+	var bitterexKey, coinbaseKey string
+	const maxArguments = 2
 	fmt.Printf("chaingang running\n")
+		for i := 1; i < len(os.Args); i += 2 {
+			if(len(os.Args) >= i + 1){
+				if(os.Args[i] == "-b"){
+					bitterexKey = os.Args[i + 1]
+				} else if(os.Args[i] == "-c"){
+					coinbaseKey = os.Args[i + 1]
+				}
+			}
+		}
+
+	fmt.Printf("\tbitterexKey: %v\n", bitterexKey)
+	fmt.Printf("\tcoinbaseKey: %v\n", coinbaseKey)
+
+	if(bitterexKey != ""){
+		fmt.Printf("time to call api\n")
+	}
 }
